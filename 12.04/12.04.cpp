77b6608344d26f56de0538d3bc5aabca640aa503
YanXv_mi56@student.itstep.org <YanXv_mi56@student.itstep.org>
2023-04-12 23:38:15 +0300

    Добавьте файлы проекта.

---
+++ 12.04/12.04.cpp
@@ -0,0 +1,20 @@
+﻿// 12.04.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "\t\tApril 2023";
+    cout << "\nMo\tTu\tWe\tTh\tFr\tSa\tSu";
+    cout << "\n\t\t\t\t\t1\t2";
+    cout << "\n3\t4\t5\t6\t7\t8\t9";
+    cout << "\n10\t11\t12\t13\t14\t15\t16";
+    cout << "\n17\t18\t19\t20\t21\t22\t23";
+    cout << "\n24\t25\t26\t27\t28\t29\t30";
+    cout << "\n31";
+}
+
+//cout << "\n";
