815dff620757e44e414ebb7e3d158bb0feed1f9f
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-04-13 22:56:32 +0300

    Add files via upload

---
+++ 12.04(4)/12.04(4)/12.04(4).cpp
@@ -0,0 +1,11 @@
+﻿// 12.04(4).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "\n\t*\t*\n\n\t\t\t*\n\n\n\t\t\t\t*\n\t\t\t\t\t\t*\n\n\n\t\t\t\t*\n\t\t\t\t\t\t*\n\n";
+}
