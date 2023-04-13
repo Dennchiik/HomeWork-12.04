801c4d2b0421e00b25fdcf499dd5d87b24dcbf7d
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-04-13 20:27:53 +0300

    Add files via upload

---
+++ 12.04(2)/12.04(2)/12.04(2).cpp
@@ -0,0 +1,21 @@
+﻿// 12.04(2).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main() {
+	std::cout << "        ___  /\\ " << std::endl;
+	std::cout << "        | | /  \\ " << std::endl;
+	std::cout << "        | |/    \\ " << std::endl;
+	std::cout << "        | /      \\ " << std::endl;
+	std::cout << "         /   00   \\ " << std::endl;
+	std::cout << "        /          \\ " << std::endl;
+	std::cout << "       /            \\ " << std::endl;
+	std::cout << "      ||============||  " << std::endl;
+	std::cout << "      ||            ||" << std::endl;
+	std::cout << "      ||     __     ||" << std::endl;
+	std::cout << "      || #   ||   # ||  || || || || || || || || || ||" << std::endl;
+	std::cout << "      ||     ||     ||  || || || || || || || || || || " << std::endl;
+	std::cout << "      ||============||  || || || || || || || || || ||  " << std::endl;
+	return 0;
+}
---
+++ 12.04(5)/12.04(5)/12.04(5).cpp
@@ -0,0 +1,12 @@
+﻿// 12.04(5).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{ 
+      cout << "\n\t*\t*\n\n\t\t\t*\n\n\n\t\t\t\t*\n\t\t\t\t\t\t*\n\n\n\t\t\t\t*\n\t\t\t\t\t\t*";
+}
+
